c6ef5de2d52197ab808ec830795c1518a437225c
Jez <jezdavisuk@gmail.com>
2024-09-30 11:28:55 +0100

    feat: add configuration files

---
+++ src/main/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.Jeneric_Java.calendarappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jeneric_Java.calendarappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
