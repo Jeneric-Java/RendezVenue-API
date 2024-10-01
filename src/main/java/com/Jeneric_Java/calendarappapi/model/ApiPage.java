3aa7bbca88af111e7f52b77c51a52d003a58d388
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-01 16:38:35 +0100

    feat: add embedded internal record to apipage

--- src/main/java/com/Jeneric_Java/calendarappapi/model/ApiPage.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/ApiPage.java
@@ -3,5 +3,6 @@ package com.Jeneric_Java.calendarappapi.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ApiPage() {
+public record ApiPage(Embedded _embedded) {
+    public record Embedded(ApiEvent[] events){}
 }
