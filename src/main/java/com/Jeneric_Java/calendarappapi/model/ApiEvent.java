97972fd8aedceee147b48f34b384d3b183c6c02c
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-01 16:33:26 +0100

    feat: add classifications internal record for api event

--- src/main/java/com/Jeneric_Java/calendarappapi/model/ApiEvent.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/ApiEvent.java
@@ -4,4 +4,8 @@ public record ApiEvent() {
     public record Dates(Date start){
         public record Date(String localTime, String localData){}
     }
+
+    public record Classifications(Segment segment){
+        public record Segment(String name){}
+    }
 }
