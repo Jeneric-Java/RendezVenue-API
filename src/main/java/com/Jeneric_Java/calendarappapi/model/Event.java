dfa41e62fdb06f34ce098ac4638823475d456cf6
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-01 16:44:51 +0100

    feat: add event class

---
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/Event.java
@@ -0,0 +1,17 @@
+package com.Jeneric_Java.calendarappapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Event{
+    long id;
+    String title;
+    String description;
+    String location;
+    String url;
+    String type;
+}
