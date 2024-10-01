5386b278ae3a5a5e00ad5b3054bcabe2f0c3d55a
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-01 16:32:31 +0100

    feat: add dates internal record for api event

--- src/main/java/com/Jeneric_Java/calendarappapi/model/ApiEvent.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/ApiEvent.java
@@ -1,4 +1,7 @@
 package com.Jeneric_Java.calendarappapi.model;
 
 public record ApiEvent() {
+    public record Dates(Date start){
+        public record Date(String localTime, String localData){}
+    }
 }
