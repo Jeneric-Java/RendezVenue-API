27e37067fc4df12a8c5dea10d6e1fe2d0f902324
JannelJ <janneljones26@gmail.com>
2024-10-02 11:10:03 +0100

    wip: api service implemtation with methods for http requests

--- src/main/java/com/Jeneric_Java/calendarappapi/service/ApiServiceImpl.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/service/ApiServiceImpl.java
@@ -1,4 +1,23 @@
 package com.Jeneric_Java.calendarappapi.service;
 
-public class ApiServiceImpl {
+import com.Jeneric_Java.calendarappapi.model.Event;
+
+public class ApiServiceImpl implements ApiService {
+
+    @Override
+    public Event getEventByID(String id) {
+        return null;
+    }
+
+    @Override
+    public Event deleteEventById(String id) {
+        return null;
+    }
+
+    @Override
+    public Event getAllEvents(String location, String title) {
+        return null;
+    }
+
+
 }
