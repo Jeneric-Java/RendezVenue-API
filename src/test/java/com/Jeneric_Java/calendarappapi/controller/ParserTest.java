7cbcc76149013f862bf53de71fdead42994a4d31
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-02 14:50:46 +0100

    style: remove unused import

--- src/test/java/com/Jeneric_Java/calendarappapi/controller/ParserTest.java
+++ src/test/java/com/Jeneric_Java/calendarappapi/controller/ParserTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
