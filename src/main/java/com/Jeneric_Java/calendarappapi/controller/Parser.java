56b3a906516274d84a9108dd3cb877f236dc3920
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-02 12:20:32 +0100

    feat: implement parser for events from api

--- src/main/java/com/Jeneric_Java/calendarappapi/controller/Parser.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/controller/Parser.java
@@ -1,13 +1,33 @@
 package com.Jeneric_Java.calendarappapi.controller;
 
 import com.Jeneric_Java.calendarappapi.model.ApiEvent;
+import com.Jeneric_Java.calendarappapi.model.Event;
 import com.Jeneric_Java.calendarappapi.model.Time;
 import org.springframework.stereotype.Controller;
 
 import java.text.ParseException;
+import java.util.stream.Stream;
 
 @Controller
 public class Parser {
+
+    public Event parseEvent(ApiEvent input) throws ParseException {
+        if (input == null) throw new IllegalArgumentException("Event cannot be null!");
+        if (input.name() == null || input.url() == null || input.dates() == null || input.classifications() == null || input.classifications().length == 0 || input.classifications()[0] == null || input.classifications()[0].segment() == null || input.classifications()[0].segment().name() == null || input._embedded() == null || input._embedded().venues() == null || input._embedded().venues().length == 0 || input._embedded().venues()[0] == null || input._embedded().venues()[0].name() == null || input._embedded().venues()[0].postalCode() == null) throw new IllegalArgumentException();
+
+        String description = input.name() + " @ " + input._embedded().venues()[0].name();
+
+        return new Event(
+                null,
+                input.name(),
+                description,
+                input._embedded().venues()[0].postalCode(),
+                input.url(),
+                input.classifications()[0].segment().name(),
+                parseTime(input.dates().start()),
+                null
+        );
+    }
 
     public Time parseTime(ApiEvent.Dates.Date input) throws ParseException {
         if (input == null || input.localTime() == null || input.localDate() == null) throw new IllegalArgumentException("Error while parsing event date/time! Date/Time cannot be null!");
