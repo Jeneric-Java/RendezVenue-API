813333920e5d87d7378b1763d2416f74176852d4
JannelJ <janneljones26@gmail.com>
2024-10-03 12:19:28 +0100

    test: remove time json from test

--- src/test/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplicationTests.java
+++ src/test/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplicationTests.java
@@ -3,12 +3,15 @@ package com.Jeneric_Java.calendarappapi;
 import com.Jeneric_Java.calendarappapi.model.Event;
 import com.Jeneric_Java.calendarappapi.model.Time;
 import com.Jeneric_Java.calendarappapi.service.ApiService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -20,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,8 +31,11 @@ public class CalendarAppApiApplicationTests {
 
 
 
+
 	@Autowired
 	private MockMvc mockMvc;
+
+	ObjectMapper mapper = new ObjectMapper();
 
 	@MockBean
 	ApiService apiService;
@@ -43,8 +50,14 @@ public class CalendarAppApiApplicationTests {
 				);
 
 				when(apiService.getAllEvents("location1")).thenReturn(mockEvents);
-				mockMvc.perform(get("/api/events/all")
-								.param("location", "location1"))
+
+				var test = mockMvc.perform(get("/api/events")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("location", "location1"));
+
+				mockMvc.perform(get("/api/events")
+					.contentType(MediaType.APPLICATION_JSON)
+							.param("location", "location1"))
 						.andExpect(status().isOk())
 						.andExpect(jsonPath("$[0].id").value(1))
 						.andExpect(jsonPath("$[0].title").value("title1"))
@@ -52,24 +65,17 @@ public class CalendarAppApiApplicationTests {
 						.andExpect(jsonPath("$[0].location").value("location1"))
 						.andExpect(jsonPath("$[0].url").value("url1"))
 						.andExpect(jsonPath("$[0].type").value("type1"))
-						.andExpect(jsonPath("$[0].startTime.hours").value(12))
-						.andExpect(jsonPath("$[0].startTime.minutes").value(5))
-						.andExpect(jsonPath("$[1].id").value(2))
 						.andExpect(jsonPath("$[1].title").value("title2"))
 						.andExpect(jsonPath("$[1].description").value("description2"))
 						.andExpect(jsonPath("$[1].location").value("location2"))
 						.andExpect(jsonPath("$[1].url").value("url2"))
 						.andExpect(jsonPath("$[1].type").value("type2"))
-						.andExpect(jsonPath("$[1].startTime.hours").value(7))
-						.andExpect(jsonPath("$[1].startTime.minutes").value(30))
 						.andExpect(jsonPath("$[2].id").value(3))
 						.andExpect(jsonPath("$[2].title").value("title3"))
 						.andExpect(jsonPath("$[2].description").value("description3"))
 						.andExpect(jsonPath("$[2].location").value("location3"))
 						.andExpect(jsonPath("$[2].url").value("url3"))
-						.andExpect(jsonPath("$[2].type").value("type3"))
-						.andExpect(jsonPath("$[2].startTime.hours").value(0))
-						.andExpect(jsonPath("$[2].startTime.minutes").value(0));
+						.andExpect(jsonPath("$[2].type").value("type3"));
 
 
 
