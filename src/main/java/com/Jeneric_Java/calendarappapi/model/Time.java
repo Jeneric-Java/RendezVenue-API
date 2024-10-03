6d2a6b7cec5a47ba89df8c34e562fd366f6d266e
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-03 15:13:28 +0100

    fix: switch column names from reserved words, implement serializable in time

--- src/main/java/com/Jeneric_Java/calendarappapi/model/Time.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/Time.java
@@ -7,31 +7,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 @Entity
 @Table(name = "Times")
-public class Time {
+public class Time implements Serializable {
     @Id
     @GeneratedValue
     @Column(name = "time_id", updatable = false, nullable = false)
     Long id;
 
-    @Column(name = "year", nullable = false)
+    @Column(name = "date_year", nullable = false)
     Integer year;
 
-    @Column(name = "month", nullable = false)
+    @Column(name = "date_month", nullable = false)
     Integer month;
 
-    @Column(name = "day", nullable = false)
+    @Column(name = "date_day", nullable = false)
     Integer day;
 
-    @Column(name = "hour", nullable = true)
+    @Column(name = "time_hour", nullable = true)
     Integer hour;
 
-    @Column(name = "minute", nullable = true)
+    @Column(name = "time_minute", nullable = true)
     Integer minute;
 
     @JsonIgnore
