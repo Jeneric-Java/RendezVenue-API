10aff141a5115d7ede3c3bfe69f48d7cd0c7ea36
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-03 14:38:42 +0100

    feat: update time with annotations

--- src/main/java/com/Jeneric_Java/calendarappapi/model/Time.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/Time.java
@@ -1,17 +1,41 @@
 package com.Jeneric_Java.calendarappapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
+@Entity
+@Table(name = "Times")
 public class Time {
+    @Id
+    @GeneratedValue
+    @Column(name = "time_id", updatable = false, nullable = false)
     Long id;
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
+
+    @Column(name = "year", nullable = false)
+    Integer year;
+
+    @Column(name = "month", nullable = false)
+    Integer month;
+
+    @Column(name = "day", nullable = false)
+    Integer day;
+
+    @Column(name = "hour", nullable = true)
+    Integer hour;
+
+    @Column(name = "minute", nullable = true)
+    Integer minute;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "event", referencedColumnName = "event_id")
+    Event event;
 }
