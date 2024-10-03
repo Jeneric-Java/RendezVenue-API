93bcf04ad0e5b43e8b1cee51be0044c3413253ce
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-03 14:39:03 +0100

    feat: update event with annotations and use eventype enum

--- src/main/java/com/Jeneric_Java/calendarappapi/model/Event.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/Event.java
@@ -1,19 +1,51 @@
 package com.Jeneric_Java.calendarappapi.model;
 
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
+@Entity
+@Table(name = "Events")
 public class Event{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "event_id", updatable = false, nullable = false)
     Long id;
+
+    @Column(name = "event_title", nullable = false)
     String title;
+
+    @Column(name = "description", nullable = true)
     String description;
+
+    @Column(name = "location", nullable = false)
     String location;
+
+    @Column(name = "event_url", nullable = true)
     String url;
-    String type;
+
+    @Column(name = "event_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    EventType type;
+
+    @Column(name = "start_time", nullable = true)
     Time startTime;
+
+    @Column(name = "end_time", nullable = true)
     Time endTime;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "event")
+    Set<Time> times = new HashSet<>();
 }
