da241b4413c51b80a9ded39e7826f9fe9c4479ed
JannelJ <janneljones26@gmail.com>
2024-10-03 11:24:33 +0100

    test: tests for the event data

--- src/test/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplicationTests.java
+++ src/test/java/com/Jeneric_Java/calendarappapi/CalendarAppApiApplicationTests.java
@@ -10,12 +10,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,7 +43,35 @@ public class CalendarAppApiApplicationTests {
 				);
 
 				when(apiService.getAllEvents("location1")).thenReturn(mockEvents);
-				mockMvc.perform(get("/api/events"));
+				mockMvc.perform(get("/api/events/all")
+								.param("location", "location1"))
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$[0].id").value(1))
+						.andExpect(jsonPath("$[0].title").value("title1"))
+						.andExpect(jsonPath("$[0].description").value("description1"))
+						.andExpect(jsonPath("$[0].location").value("location1"))
+						.andExpect(jsonPath("$[0].url").value("url1"))
+						.andExpect(jsonPath("$[0].type").value("type1"))
+						.andExpect(jsonPath("$[0].startTime.hours").value(12))
+						.andExpect(jsonPath("$[0].startTime.minutes").value(5))
+						.andExpect(jsonPath("$[1].id").value(2))
+						.andExpect(jsonPath("$[1].title").value("title2"))
+						.andExpect(jsonPath("$[1].description").value("description2"))
+						.andExpect(jsonPath("$[1].location").value("location2"))
+						.andExpect(jsonPath("$[1].url").value("url2"))
+						.andExpect(jsonPath("$[1].type").value("type2"))
+						.andExpect(jsonPath("$[1].startTime.hours").value(7))
+						.andExpect(jsonPath("$[1].startTime.minutes").value(30))
+						.andExpect(jsonPath("$[2].id").value(3))
+						.andExpect(jsonPath("$[2].title").value("title3"))
+						.andExpect(jsonPath("$[2].description").value("description3"))
+						.andExpect(jsonPath("$[2].location").value("location3"))
+						.andExpect(jsonPath("$[2].url").value("url3"))
+						.andExpect(jsonPath("$[2].type").value("type3"))
+						.andExpect(jsonPath("$[2].startTime.hours").value(0))
+						.andExpect(jsonPath("$[2].startTime.minutes").value(0));
+
+
 
 	}
 
@@ -53,4 +85,4 @@ public class CalendarAppApiApplicationTests {
 	}
 
 
-}
+
