c3dbc598ad9f2287f2c56a4c221a2b37495cf365
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-03 18:44:49 +0100

    feat: update parser to handle expected missing info from external api

--- src/main/java/com/Jeneric_Java/calendarappapi/controller/Parser.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/controller/Parser.java
@@ -2,7 +2,6 @@ package com.Jeneric_Java.calendarappapi.controller;
 
 import com.Jeneric_Java.calendarappapi.exception.NoResultsFoundException;
 import com.Jeneric_Java.calendarappapi.model.*;
-import com.google.common.collect.ImmutableMap;
 import org.springframework.stereotype.Controller;
 
 import java.text.ParseException;
@@ -13,17 +12,51 @@ import java.util.List;
 public class Parser {
 
     public Event parseEvent(ApiEvent input) throws ParseException {
-        if (input == null || input.hasNullFields()) throw new IllegalArgumentException("Event cannot be null!");
+        if (input == null
+                || input.name() == null
+                || input.url() == null
+                || input.dates() == null
+                || input.dates().start() == null) {
+            throw new IllegalArgumentException("Event cannot be null!");
+        }
 
-        String description = input.name() + " @ " + input._embedded().venues()[0].name();
+        String name = input.name();
+
+        String postalCode;
+        if (input._embedded() == null
+                || input._embedded().venues() == null
+                || input._embedded().venues().length == 0) {
+            postalCode = "No Location Given";
+        } else {
+            postalCode = input._embedded().venues()[0].postalCode() != null
+                    ? input._embedded().venues()[0].postalCode()
+                    : "No Location Given";
+        }
+
+        String url = input.url();
+
+        EventType type =
+                (input.classifications() != null && input.classifications()[0].segment() != null)
+                        ? parseSegment(input.classifications()[0].segment())
+                        : EventType.MISC;
+
+        String description;
+        if (input._embedded() == null
+                || input._embedded().venues() == null
+                || input._embedded().venues().length == 0
+                || input._embedded().venues()[0].name() == null) {
+            description = "Event (" + type + "): " + input.name();
+        } else {
+            description = input.name() + " @ " + input._embedded().venues()[0].name();
+        }
 
         return new Event(
                 null,
-                input.name(),
+                name,
                 description,
-                input._embedded().venues()[0].postalCode(),
-                input.url(),
-                parseSegment(input.classifications()[0].segment()),
+                postalCode,
+                url,
+                type,
                 parseTime(input.dates().start()),
                 null,
                 null
@@ -31,36 +64,48 @@ public class Parser {
     }
 
     private EventType parseSegment(ApiEvent.Classifications.Segment segment) {
-        final ImmutableMap<String, EventType> eventTypes = ImmutableMap.of(
-                "KZFzniwnSyZfZ7v7na", EventType.ART_THEATRE,
-                "KZFzniwnSyZfZ7v7n1", EventType.MISC,
-                "KZFzniwnSyZfZ7v7nE", EventType.SPORT,
-                "KZFzniwnSyZfZ7v7nJ", EventType.MUSIC,
-                "KZFzniwnSyZfZ7v7nn", EventType.FILM
-        );
         if (segment.id() == null) return EventType.MISC;
-        return eventTypes.getOrDefault(segment.id(), EventType.MISC);
+
+        return switch (segment.id()) {
+            case "KZFzniwnSyZfZ7v7na" -> EventType.ART_THEATRE;
+            case "KZFzniwnSyZfZ7v7nE" -> EventType.SPORT;
+            case "KZFzniwnSyZfZ7v7nJ" -> EventType.MUSIC;
+            case "KZFzniwnSyZfZ7v7nn" -> EventType.FILM;
+            default -> EventType.MISC;
+        };
     }
 
     public Time parseTime(ApiEvent.Dates.Date input) throws ParseException {
-        if (input == null || input.hasNullFields()) throw new IllegalArgumentException("Error while parsing event date/time! Date/Time cannot be null!");
+        if (input == null) throw new IllegalArgumentException("Error while parsing event date/time! Date/Time cannot be null!");
 
-        String[] date = input.localDate().split("-");
-        String[] time = input.localTime().split(":");
+        Integer year, month, day, hour, minute;
 
-        try {
-            return new Time(
-                    null,
-                    Integer.parseInt(date[0]),
-                    Integer.parseInt(date[1]),
-                    Integer.parseInt(date[2]),
-                    Integer.parseInt(time[0]),
-                    Integer.parseInt(time[1]),
-                    null
-            );
-        } catch (NumberFormatException e) {
-            throw new ParseException("Error while parsing event date/time!", -1);
+        if (input.localDate() == null || input.localDate().isBlank()) {
+            throw new IllegalArgumentException("Date cannot be null!");
+        } else {
+            String[] date = input.localDate().split("-");
+            try {
+                year = Integer.parseInt(date[0]);
+                month = Integer.parseInt(date[1]);
+                day = Integer.parseInt(date[2]);
+            } catch (NumberFormatException e) {
+                throw new ParseException("Error while parsing event date!", -1);
+            }
         }
+
+        if (input.localTime() == null || input.localTime().isBlank()) {
+            hour = minute = null;
+        } else {
+            String[] time = input.localTime().split(":");
+            try {
+                hour = Integer.parseInt(time[0]);
+                minute = Integer.parseInt(time[1]);
+            } catch (NumberFormatException e) {
+                throw new ParseException("Error while parsing event time!", -1);
+            }
+        }
+
+        return new Time(null, year, month, day, hour, minute, null);
     }
 
     public List<Event> parsePage(ApiPage input) throws ParseException {
