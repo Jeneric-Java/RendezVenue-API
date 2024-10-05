82b2206d29e86e485b2d3cab487ed95746169e57
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-05 17:29:21 +0100

    delete time classes

--- src/main/java/com/Jeneric_Java/calendarappapi/repository/TimeRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.Jeneric_Java.calendarappapi.repository;
-//
-//import com.Jeneric_Java.calendarappapi.model.Event;
-//import com.Jeneric_Java.calendarappapi.model.Time;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface TimeRepository extends CrudRepository<Time, Long> {
-//
-//}
