1951af2c2ac15d4fc2cf7a519c49fd4d090a90f5
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-05 17:33:30 +0100

    refactor: rename api event/page to ticketmaster event/page

--- src/main/java/com/Jeneric_Java/calendarappapi/model/TicketmasterEvent.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/TicketmasterEvent.java
@@ -3,7 +3,7 @@ package com.Jeneric_Java.calendarappapi.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ApiEvent(String name, String url, Dates dates, Classifications[] classifications, Embedded _embedded) {
+public record TicketmasterEvent(String name, String url, Dates dates, Classifications[] classifications, Embedded _embedded) {
     public record Dates(Date start){
       public record Date(String localTime, String localDate){}
     }
--- src/main/java/com/Jeneric_Java/calendarappapi/model/TicketmasterPage.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/model/TicketmasterPage.java
@@ -3,8 +3,8 @@ package com.Jeneric_Java.calendarappapi.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ApiPage(Embedded _embedded, Page page) {
-    public record Embedded(ApiEvent[] events){}
+public record TicketmasterPage(Embedded _embedded, Page page) {
+    public record Embedded(TicketmasterEvent[] events){}
 
     public record Page(int size, int totalElements, int totalPages, int number){}
 }
