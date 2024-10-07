ae5d3368c13dd3fbdc8dd089d94b87a6744f52f4
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-07 16:37:45 +0100

    feat: add swagger annotations for apicontroller

--- src/main/java/com/Jeneric_Java/calendarappapi/controller/ApiController.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/controller/ApiController.java
@@ -2,6 +2,9 @@ package com.Jeneric_Java.calendarappapi.controller;
 
 import com.Jeneric_Java.calendarappapi.model.Event;
 import com.Jeneric_Java.calendarappapi.service.ApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,32 +23,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@Tag(name = "RendezVenue API", description = "Find events across the uk")
 public class ApiController {
 
     @Autowired
     private ApiService apiService;
 
     // GET all events by location
+    @Operation(summary = "Get a list of events from results in a given area")
     @GetMapping()
-    public ResponseEntity<List<Event>> getEventsByLocation(@RequestParam String geoHashEnc) throws InvalidAlgorithmParameterException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, IOException, InvalidKeyException {
+    public ResponseEntity<List<Event>> getEventsByLocation(@RequestParam @Parameter(name = "geoHashEnc", description = "Encoded geoHash of the area to be searched", required = true) String geoHashEnc) throws InvalidAlgorithmParameterException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, IOException, InvalidKeyException {
         return new ResponseEntity<>(apiService.getEventsByLocation(geoHashEnc), HttpStatus.OK);
     }
 
     // GET event by id
+    @Operation(summary = "Get info on a specific event in our database by id")
     @GetMapping("/{id}")
-    public ResponseEntity<Event> getEventById(@PathVariable Long id){
+    public ResponseEntity<Event> getEventById(@PathVariable @Parameter(name = "id", description = "Id of the event to be retrieved") Long id){
         return new ResponseEntity<>(apiService.getEventByID(id), HttpStatus.OK);
     }
 
     // DELETE event by id
+    @Operation(summary = "Delete a specific event in our database by id")
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteEventById(@PathVariable Long id){
+    public ResponseEntity<String> deleteEventById(@PathVariable @Parameter(name = "id", description = "Id of the event to be deleted") Long id){
         return new ResponseEntity<>(apiService.deleteEventById(id), HttpStatus.OK);
     }
 
     // POST event
+    @Operation(summary = "Create a new event in our database")
     @PostMapping
-    public ResponseEntity<Event> addEvent(@RequestBody Event event) {
+    public ResponseEntity<Event> addEvent(@RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Event to be added to the database") Event event) {
         Event newEvent  = apiService.insertEvent(event);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("event", "/api/events/" + newEvent.getId().toString());
@@ -53,8 +61,10 @@ public class ApiController {
     }
 
     // UPDATE event
+    @Operation(summary = "Update the info on an event in our database by id")
     @PatchMapping("/{id}")
-    public ResponseEntity<Event> updateEventById(@PathVariable("id") Long id, @RequestBody Event event) {
+    public ResponseEntity<Event> updateEventById(@PathVariable("id") @Parameter(name = "id", description = "Id of the event to be updated") Long id,
+                                                 @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Updated event to be put in the database") Event event) {
         return new ResponseEntity<>(apiService.updateEventById(id, event), HttpStatus.NO_CONTENT);
     }
 }
