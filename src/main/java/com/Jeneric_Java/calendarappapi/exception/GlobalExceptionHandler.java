5fdf44ce4a3af54afdc578b93077511e60f7783f
jmc <120180071+sector-11@users.noreply.github.com>
2024-10-08 17:42:18 +0100

    feat: add handling for propertyvalueexceptions

--- src/main/java/com/Jeneric_Java/calendarappapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Jeneric_Java/calendarappapi/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.Jeneric_Java.calendarappapi.exception;
 
+import org.hibernate.PropertyValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<Object> handleItemNotFoundException(NoResultsFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handlePropertyValueException(PropertyValueException exception) {
+        return new ResponseEntity<>("Bad Request! Event must contain title, location, type, closestCity, and startDate", HttpStatus.BAD_REQUEST);
     }
 }
